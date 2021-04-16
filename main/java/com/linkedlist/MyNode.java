eba8da041b3d8eab13fad09ab12e5fcf7e99b1a7
Deva88 <devendra.raj.sdm@gmail.com>
2021-04-16 17:19:51 +0530

    Added: Welcome To Linked List

---
+++ main/java/com/linkedlist/MyNode.java
@@ -0,0 +1,20 @@
+package com.linkedlist;
+/*
+ *@Description:-create a simple Linked List
+ */
+public class MyNode<K> {
+    private MyNode next;
+    private K key;
+
+    public MyNode getNext() {
+        return next;
+    }
+
+    public void setNext(MyNode next) {
+        this.next = next;
+    }
+    public MyNode(K key) {
+        this.key = key;
+        this.next = next;
+    }
+}
